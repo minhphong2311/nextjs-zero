3b465cf2845454444c03b95fa608c5d9955a89f1
minhphong2311 <minhphong2311@gmail.com>
2023-09-29 14:37:40 +0700

    put data

--- src/components/app.table.tsx
+++ src/components/app.table.tsx
@@ -3,48 +3,64 @@ import Table from 'react-bootstrap/Table';
 import { Button } from 'react-bootstrap';
 import CreateModal from './create.modal';
 import { useState } from 'react';
+import UpdateModal from './update.modal';
 
-interface IProps{
+interface IProps {
   blogs: IBlog[]
 }
 
 const AppTable = (props: IProps) => {
   const { blogs } = props;
-  
-  const [showModalCreate, setShowModalCreate] = useState<boolean>(false)
+
+  const [blog, setBlog] = useState<Iblog | null>(null);
+  const [showModalCreate, setShowModalCreate] = useState<boolean>(false);
+  const [showModalUpdate, setShowModalUpdate] = useState<boolean>(false);
+
   return (
     <>
-    <div className='mb-3' style={{display: 'flex', justifyContent:'space-between'}}>
-      <h3>Table Blogs</h3>
-      <Button variant='secondary' onClick={() => setShowModalCreate(true)}>Add New</Button>
-    </div>
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {blogs.map(blog=>{
-          return (
-          <tr key={blog.id}>
-            <td>{blog.id}</td>
-            <td>{blog.title}</td>
-            <td>{blog.author}</td>
-            <td>
-              <Button>View</Button>
-              <Button variant='warning' className='mx-3'>Edit</Button>
-              <Button variant='danger'>Delete</Button>
-            </td>
+      <div className='mb-3' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h3>Table Blogs</h3>
+        <Button variant='secondary' onClick={() => setShowModalCreate(true)}>Add New</Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
           </tr>
-          )
-        })}
-      </tbody>
-    </Table>
-    <CreateModal showModalCreate={showModalCreate} setShowModalCreate={setShowModalCreate} />
+        </thead>
+        <tbody>
+          {blogs.map(item => {
+            return (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.author}</td>
+                <td>
+                  <Button>View</Button>
+                  <Button variant='warning' className='mx-3' onClick={() => {
+                    setBlog(item);
+                    setShowModalUpdate(true);
+                  }}>Edit</Button>
+                  <Button variant='danger'>Delete</Button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+      <CreateModal
+        showModalCreate={showModalCreate}
+        setShowModalCreate={setShowModalCreate}
+      />
+      <UpdateModal
+        showModalUpdate={showModalUpdate}
+        setShowModalUpdate={setShowModalUpdate}
+        blog={blog}
+        setBlog={setBlog}
+      />
     </>
   );
 }
